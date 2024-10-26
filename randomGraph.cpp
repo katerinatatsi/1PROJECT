8ae7d14b105dca7a25bbed10f727abec336dc82a
katerinatatsi <katerinatatsi63@gmail.com>
2024-10-26 15:13:13 +0300

    Προσθήκη medoid στον vamana  αλγόριθμο

--- randomGraph.cpp
+++ randomGraph.cpp
@@ -1,13 +1,57 @@
 #include <iostream>
 #include <iomanip>
-
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <fstream>
 #define N 5 // Αριθμός κόμβων (μπορείς να αλλάξεις αυτόν τον αριθμό)
 #define R 4   // Κανονικότητα (in-degree και out-degree, μπορείς να το αλλάξεις)
+#define D 128 // Διαστάσεις
 
 using namespace std;
 
+//για medoid
+
+struct PointMedoid {
+    float coords[D];  // Συντεταγμένες στον 128-διάστατο χώρο
+};
+
+// Συνάρτηση που υπολογίζει την Ευκλείδεια απόσταση μεταξύ δύο σημείων
+double euclidean_distance(const PointMedoid& a, const PointMedoid& b) {
+    double sum = 0.0;
+    for (int i = 0; i < D; i++) {
+        sum += (a.coords[i] - b.coords[i]) * (a.coords[i] - b.coords[i]);
+    }
+    return sqrt(sum);
+}
+
+// Συνάρτηση που βρίσκει το medoid με τη μέθοδο brute force
+int find_medoid(const vector<PointMedoid>& points) {
+    int n = points.size();
+    double min_total_distance = numeric_limits<double>::infinity();
+    int medoid_index = -1;
+
+    for (int i = 0; i < n; i++) {
+        double total_distance = 0.0;
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                total_distance += euclidean_distance(points[i], points[j]);
+            }
+        }
+        if (total_distance < min_total_distance) {
+            min_total_distance = total_distance;
+            medoid_index = i;
+        }
+    }
+    return medoid_index;
+}
+
+
+//τελος medoid
+
 struct Point {
-    float coords[28];  // Συντεταγμένες στον 28-διάστατο χώρο
+    float coords[128];  // Συντεταγμένες στον 28-διάστατο χώρο
 };
 
 // Συνάρτηση για την εμφάνιση του πίνακα γειτνίασης
@@ -85,6 +129,42 @@ int main() {
     } else {
         cout << "\nThe graph is NOT " << R << "-regular.\n";
     }
+
+    //medoid
+
+    vector<PointMedoid> pointVec(N);
+    ifstream file("points.txt");
+    if (!file) {
+        cerr << "Could not open the file!" << endl;
+        return 1;
+    }
+
+    // Διαβάζουμε τα σημεία από το αρχείο
+    for (int i = 0; i < N; i++) {
+        cout << "Reading point " << i << ": ";
+        for (int j = 0; j < D; j++) {
+            if (!(file >> pointVec[i].coords[j])) {
+                cerr << "Error reading coordinates at point " << i << ", coordinate " << j << endl;
+                return 1;
+            }
+            cout << pointVec[i].coords[j] << " ";
+        }
+        cout << endl;
+    }
+    file.close();
+
+    // Βρίσκουμε το medoid
+    int medoid_index = find_medoid(pointVec);
+
+    // Εκτύπωση του Medoid
+    cout << "\nMedoid Index: " << medoid_index << endl;
+    cout << "Medoid Coordinates: ";
+    for (int j = 0; j < D; j++) {
+        cout << fixed << setprecision(2) << pointVec[medoid_index].coords[j] << " ";
+    }
+    cout << endl;
+
+    //τελος medoid
 
     return 0;
 }
