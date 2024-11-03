e6268aa1ea17fae9f8e715dbdede48da3c363987
kostisver <61734556+kostisver@users.noreply.github.com>
2024-11-03 20:26:26 +0200

    Add files via upload
    Updated greedy search algorithm

---
+++ src/hpp/greedy_search.hpp
@@ -0,0 +1,20 @@
+#ifndef GREEDY_SEARCH_HPP
+#define GREEDY_SEARCH_HPP
+
+#include <vector>
+#include <set>
+
+#include "Point.hpp"
+#include "Node.hpp"
+
+using namespace std;
+
+pair<vector<int>, set<int>> greedySearch(
+    const vector<Node>& graph,
+    int startNodeId, 
+    const Point& queryPoint, 
+    int k, 
+    int L
+);
+
+#endif // GREEDY_SEARCH_HPP
